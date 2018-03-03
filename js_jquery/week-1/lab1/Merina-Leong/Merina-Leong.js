43b3f747ad22e7c4d72534ef14e85dbdedf4bc96
Merina <merinaleong@gmail.com>
2018-03-03 15:48:28 -0800

    week 2 notes

---
+++ js_jquery/week-1/lab1/Merina-Leong/Merina-Leong.js
@@ -0,0 +1,15 @@
+/*
+Merina Leong
+A00988891
+Lab 1
+*/
+
+function catSize(){
+  var width = document.getElementById("cat").width;
+  document.getElementById("bird").setAttribute("width", width);
+}
+
+function dogSize(){
+  var width = document.getElementById("cat").width;
+  document.getElementById("dog").setAttribute("width", width);
+}
